f5ecde1b45830b12bdcfdaccdb6ce93ec67c9689
NewNamesAreHard <35807013+NewNamesAreHard@users.noreply.github.com>
2023-10-01 05:22:45 +0200

    First commit
    first draft, should work

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("timestampForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const selectedDate = document.getElementById("datepicker").value;
+        const selectedTime = document.getElementById("timepicker").value;
+
+        const selectedDateTime = new Date(selectedDate + " " + selectedTime);
+        const unixTimestamp = Math.floor(selectedDateTime.getTime() / 1000);
+
+        document.getElementById("timestamp").textContent = unixTimestamp;
+
+        const formattedTimestamp = `<t:${unixTimestamp}:F>`;
+        document.getElementById("formattedTimestamp").value = formattedTimestamp;
+    });
+});
+
+function copyToClipboard() {
+    const formattedTimestampField = document.getElementById("formattedTimestamp");
+    formattedTimestampField.select();
+    document.execCommand("copy");
+}
